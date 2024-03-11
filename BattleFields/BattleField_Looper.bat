b7991b67d15ce4ed6f4a04de286d445cf1c40bdb
illidan <88918931+JTNeXuS2@users.noreply.github.com>
2024-03-12 01:30:58 +0200

    Update BattleField_Looper.bat

--- BattleFields/BattleField_Looper.bat
+++ BattleFields/BattleField_Looper.bat
@@ -3,7 +3,7 @@ cd %~dp0
 mode con:cols=70 lines=8
 
 :start
-:: ID sesion name and read log (-ServerId and -SessionName and -log=BattleServer_XXXX.log)
+:: ID sesion name and read log 
 set "serverid=7020"
 set time=04:00:00
 
@@ -13,6 +13,16 @@ for /f "tokens=1-3 delims=/-. " %%a in ('date /t') do (
     set "month=%%b"
     set "year=%%c"
 )
+
+echo find Window: SessionName-%serverid% ServerId-%serverid% PID-
+powershell -command "$foundWindow = Get-Process | Where-Object {$_.MainWindowTitle -like '*SessionName-%serverid% ServerId-%serverid% PID-*'}; if ($foundWindow) { exit 0 } else { exit 1 }"
+if %errorlevel% equ 0 (
+    goto check_log
+) else (
+    goto run
+)
+
+:run
 
 RunAsDate.exe /immediate /movetime %day%\%month%\%year% %time% "..\WindowsPrivateServer\MOE\Binaries\Win64\MOEServer.exe" Battlefield_Main_New -game -server -ClusterId=8888 -log -StartBattleService -StartPubData -BigPrivateServer -DistrictId=1 -EnableParallelTickFunction -DisablePhysXSimulation -LOCALLOGTIMES -corelimit=5 -core -HangDuration=300 -NotCheckServerSteamAuth  -ActivityServer=true -MultiHome=65.109.113.61 -OutAddress=65.109.113.61 -Port=GAMEPORT -QueryPort=STEAMPORT -ShutDownServicePort=RCONPORT -ShutDownServiceIP=65.109.113.61 -ShutDownServiceKey=RCONPASS -MaxPlayers=100 -SessionName=7020 -ServerId=7020 log=BattleServer_7020.log -PubDataAddr=65.109.113.61 -PubDataPort=PUBPORT -DBAddr=65.109.113.61 -DBPort=DBPORT -BattleAddr=65.109.113.61 -BattlePort=BATLEPORT -ChatServerAddr=65.109.113.61 -ChatServerPort=CHATPORT -ChatClientAddress=65.109.113.61 -ChatClientPort=CLICHATPORT -OptEnable=1 -OptAddr=65.109.113.61 -OptPort=OPTPORT -Description="Discord" -MaxPlayers=100 -NoticeSelfEnable=true -NoticeSelfEnterServer="Discord" -MapDifficultyRate=1 -UseACE -EnableVACBan=1 -ServerAdminAccounts=111111;222222 -NoticeAllEnable=true -NoticeEnterServer=" Discord" -SaveGameIntervalMinute=7 -NormalReduceDurableMultiplier=0.8 -bEnableServerLevel=false -GeneralQualityMultiPVPBlue=0 -GeneralQualityMultiPVPPurse=0.2 -HorseMaxQualityCorrectionPVE=0.9 -TameAnimalLifeMultiplier=2 -MoveSeatLoadMultiplier=2 -NUM_AllGeneralMax=10 -NUM_WarGeneralMax=2 -NUM_AllHorseMax=20 -NUM_WarHorseMax=10 -TameAnimalMatingSpeedMultiplier=2 -TameAnimalMatingCDMultiplier=0.5 -BabyAnimalGrowthRateMultiplier=0.5 -ItemCraftRepairTimeMulti=0.3  -BattleServerReduceInventoryDurablePercent=0 -OccupyDecayHPMultiplier=3 
 
@@ -33,5 +43,5 @@ if %errorlevel% equ 1 (
 ) else (
     timeout /t 30
     cls
-    goto check_log
+    goto start
 )

d31fc49aec2617ec6117db55e8588f9e0db4ddc8
illidan <88918931+JTNeXuS2@users.noreply.github.com>
2024-03-13 10:27:37 +0200

    Update BattleField_Looper.bat

--- BattleFields/BattleField_Looper.bat
+++ BattleFields/BattleField_Looper.bat
@@ -5,7 +5,7 @@ setlocal enabledelayedexpansion
 
 :start
 :: ID sesion name and read log 
-set "serverid=7020"
+set "serverid=7010"
 TITLE BATTLEFIELD Looper SessionName-%serverid% ServerId-%serverid%
 
 :: looking for the official launch time of the event (https://discord.com/channels/911228503154917387/911228503721119828/1216847557804687451)
@@ -18,10 +18,16 @@ set startday=MON
 :: Friday [FRI] – пятница
 :: Saturday [SAT] – суббота
 :: Sunday [SUN] – воскресенье
-
+if "%startday%"=="MON" set "dayOfWeek=1"
+if "%startday%"=="TUE" set "dayOfWeek=2"
+if "%startday%"=="WED" set "dayOfWeek=3"
+if "%startday%"=="THU" set "dayOfWeek=4"
+if "%startday%"=="FRI" set "dayOfWeek=5"
+if "%startday%"=="SAT" set "dayOfWeek=6"
+if "%startday%"=="SUN" set "dayOfWeek=0"
 :: looking for the nearest official launch day of the event (https://discord.com/channels/786062570095116293/1171345133141237780/1216977618021388328)
 :: check the day is equal to '%startday% if not equal, look for the closest one
-for /f "tokens=1-3 delims=/-. " %%a in ('powershell -Command "$currentDate = Get-Date; if ($currentDate.DayOfWeek -ne '%startday%') { $daysUntilSaturday = (6 - $currentDate.DayOfWeek.value__); $daysUntilMonday = (1 - $currentDate.DayOfWeek.value__); if ($daysUntilMonday -lt $daysUntilSaturday) { $newDate = $currentDate.AddDays($daysUntilMonday) } else { $newDate = $currentDate.AddDays($daysUntilSaturday) }; $newDate.ToString('dd/MM/yyyy HH:mm:ss') } else { $currentDate.ToString('dd/MM/yyyy HH:mm:ss') }"') do (
+for /f "tokens=1-3 delims=/-. " %%a in ('powershell -Command "$currentDate = Get-Date; if ($currentDate.DayOfWeek -ne '%startday%') { $daysUntil = (%dayOfWeek% - $currentDate.DayOfWeek.value__); $newDate = $currentDate.AddDays($daysUntil); $newDate.ToString('dd/MM/yyyy HH:mm:ss') } else { $currentDate.ToString('dd/MM/yyyy HH:mm:ss') }"') do (
     set "day=%%a"
     set "month=%%b"
     set "year=%%c"

091c89df1d113653e1e9a77e110ceb6ad47ac5a7
⎛⎝Иллидан⎠⎞ <88918931+JTNeXuS2@users.noreply.github.com>
2024-03-29 12:31:59 +0200

    Update ShedulerRestart.cmd

--- ShedulerRestart.cmd
+++ ShedulerRestart.cmd
@@ -1,8 +1,9 @@
 @echo off
 cd %~dp0
 chcp 65001
+setlocal enabledelayedexpansion
 
-:: Run once to setup script in to windows task sheduler (taskschd.msc) 12:00 Autorun
+:: Run once to setup script in to windows task sheduler (taskschd.msc) 11:50 Autorun
 schtasks /query /tn "MOE.Restart"
 echo %errorlevel%
 if %errorlevel% == 1 (
@@ -16,36 +17,46 @@ if %errorlevel% == 1 (
 set "clusterpath=C:\moe_cluster"
 set "mcrcon_dir=%clusterpath%\rcon"
 set "ConfigFile=%clusterpath%\MatrixServerTool\ServerParamConfig_All.ini"
-set "WEBHOOK_URL=https://discord.com/api/webhooks/1210772484509671464/CwEwsl6GTV6ze16joW0sIyK62nYrPSd4OM2BjAtkgbXIUgOSJ1TGp0l0grBJc5FRXi2s"
+set "WEBHOOK_URL=https://discord.com/api/webhooks/1222834102860910603/shduashday38742y42893"
 
 :: ANNONCE
 echo Discord annonce 1
-set "MESSAGE=@here\n Плановый рестар!\n Кластер будет перезагружен через 10 минут!"
+set "MESSAGE=@here\n Плановый рестарт!\n Кластер будет перезагружен через 10 минут!"
+set "rconmessage=Рестарт_через_10_минут"
 curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"%MESSAGE%\"}" %WEBHOOK_URL%
+%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 8012 -p superpass "BroadcastNotifySysInfo %rconmessage% 1 0"
+%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 8022 -p superpass "BroadcastNotifySysInfo %rconmessage% 1 0"
+%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 8032 -p superpass "BroadcastNotifySysInfo %rconmessage% 1 0"
 timeout /t 300
 
 echo Discord annonce 2
-set "MESSAGE= Плановый рестар!\n Кластер будет перезагружен через 5 минут!"
+set "MESSAGE= Плановый рестарт!\n Кластер будет перезагружен через 5 минут!"
+set "rconmessage=Рестарт_через_5_минут"
 curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"%MESSAGE%\"}" %WEBHOOK_URL%
+%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 8012 -p superpass "BroadcastNotifySysInfo %rconmessage% 1 0"
+%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 8022 -p superpass "BroadcastNotifySysInfo %rconmessage% 1 0"
+%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 8032 -p superpass "BroadcastNotifySysInfo %rconmessage% 1 0"
 timeout /t 240
 
 echo Discord annonce 3
-set "MESSAGE=@here\n Плановый рестар!\n Кластер будет перезагружен через 1 минуту!"
+set "MESSAGE=@here\n Плановый рестарт!\n Кластер будет перезагружен через 1 минуту!"
+set "rconmessage=Рестарт_через_1_минуту"
 curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"%MESSAGE%\"}" %WEBHOOK_URL%
+%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 8012 -p superpass "BroadcastNotifySysInfo %rconmessage% 1 0"
+%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 8022 -p superpass "BroadcastNotifySysInfo %rconmessage% 1 0"
+%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 8032 -p superpass "BroadcastNotifySysInfo %rconmessage% 1 0"
 timeout /t 60
 
-setlocal enabledelayedexpansion
-
 echo Saving Worlds....
-%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 1234 -p RCON_PASS SaveWorld
-%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 1235 -p RCON_PASS SaveWorld
-%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 1236 -p RCON_PASS SaveWorld
-TIMEOUT /t 15
+%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 8012 -p superpass SaveWorld
+%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 8022 -p superpass SaveWorld
+%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 8032 -p superpass SaveWorld
+TIMEOUT /t 20
 
 echo Exiting Worlds...
-%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 1234 -p RCON_PASS ShutdownServer
-%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 1235 -p RCON_PASS ShutdownServer
-%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 1236 -p RCON_PASS ShutdownServer
+%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 8012 -p superpass ShutdownServer
+%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 8022 -p superpass ShutdownServer
+%mcrcon_dir%\mcrcon.exe -H 127.0.0.1 -P 8032 -p superpass ShutdownServer
 TIMEOUT /t 10
 
 :Secondkill
@@ -57,27 +68,13 @@ for /f "tokens=1 delims==" %%i in (%ConfigFile%) do (
         set "WindowTitle=SessionName-SceneServer_%serverid% ServerId-%serverid%"
 		::powershell.exe -command "$Processes = Get-Process; $Processes | Where-Object {$_.MainWindowTitle -like '*%WindowTitle%*'} | ForEach-Object {Write-Host $_.Id}"
         powershell.exe -command "$Processes = Get-Process; $Processes | Where-Object {$_.MainWindowTitle -like '*%WindowTitle%*'} | ForEach-Object {Stop-Process -Id $_.Id}"
+		timeout /t 0 >nul
     )
 	goto Next
 )
+
 :Next
-
-
-echo start servers
-::echo Battle
-::call "C:\moe_cluster\MatrixServerTool\StartBattleServer_7010.bat"
-::timeout /t 10
-
-echo Scene 100
-call "C:\moe_cluster\MatrixServerTool\StartSceneServer_100.bat"
-timeout /t 10
-
-echo Scene 200
-call "C:\moe_cluster\MatrixServerTool\StartSceneServer_200.bat"
-timeout /t 10
-
-echo Scene 300
-call "C:\moe_cluster\MatrixServerTool\StartSceneServer_300.bat"
-
-echo exit...
-timeout /t 10
+echo Start Claster
+call "C:\moe_cluster\scripts\^!START_CLUSTER.cmd"
+timeout /t 3
+EXIT

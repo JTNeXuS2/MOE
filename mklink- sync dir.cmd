5b93b18f290e96eb78f102c8b4379e4ea9cd29f1
⎛⎝Иллидан⎠⎞ <88918931+JTNeXuS2@users.noreply.github.com>
2025-04-08 18:55:18 +0200

    Update mklink- sync dir.cmd

--- mklink- sync dir.cmd
+++ mklink- sync dir.cmd
@@ -5,7 +5,7 @@ mklink /D /J "C:\moe_cluster\MatrixServerTool" "C:\moe_cluster\moe\MatrixServerT
 mklink "C:\moe_cluster\moe\configs\ServerParamConfig_All.ini" "C:\moe_cluster\moe\MatrixServerTool\ServerParamConfig_All.ini"
 
 set "shortcut=C:\moe_cluster\MatrixServerTool - Shortcut.lnk"
-set "workingdir=C:\moe_cluster\MatrixServerTool\"
+set "workingdir=C:\moe_cluster\moe\MatrixServerTool"
 
 set "target=%workingdir%\MatrixServerTool.exe"
 powershell -Command "$WshShell = New-Object -ComObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut('%shortcut%'); $Shortcut.TargetPath = '%target%'; $Shortcut.WorkingDirectory = '%workingdir%'; $Shortcut.Save()"
